0599b085ad3ed2dcb7191d5878a7ca6110e8fa13
Celimage <celie.pautrel@gmail.com>
2020-07-19 17:13:26 +0200

    Placement protons

--- nodes/Grid.gd
+++ nodes/Grid.gd
@@ -1,32 +1,68 @@
 extends TileMap
 
-
 export(PackedScene) var Nucleon
 
+var SIDE_LENGTH = 12
 var elements = []
+var matrix = []
+
+onready var CASE_WIDTH = round(get_viewport().get_visible_rect().size.x / SIDE_LENGTH)
 
 func _ready():
-	var matrix = []
-	for x in range(50):
-		matrix.append([])
-		for y in range(50):
-			matrix[x].append(null)
+	for x in range(SIDE_LENGTH):
+		var row = []
+		for y in range(SIDE_LENGTH):
+			row.append(null)
+		matrix.append(row)
 
 
 func _input(event):
 	if event is InputEventMouseButton && event.is_pressed():
-		$Raycast.cast_to = get_local_mouse_position()
-		if !$Raycast.get_collider():
-			spawn_nucleon(get_local_mouse_position())
+		#var posPixel = get_local_mouse_position()
+		var posPixel = event.position
+		var posCaseX = ceil(posPixel.x / CASE_WIDTH) - 1
+		var posCaseY = ceil(posPixel.y / CASE_WIDTH) - 1
+		if(matrix[posCaseX][posCaseY] == null):
+			spawn_nucleon(posPixel, Vector2(posCaseX, posCaseY))
+		
+		#$Raycast.cast_to = get_local_mouse_position()
+		#if !$Raycast.get_collider():
+		#	spawn_nucleon(get_local_mouse_position())
 
-func spawn_nucleon(pos : Vector2):
+func spawn_nucleon(posPx : Vector2, posCase : Vector2):
 	if !Nucleon: return
 	var inst = Nucleon.instance()
-	inst.position = map_to_world(world_to_map(pos))
+	#inst.position = map_to_world(world_to_map(posPx))
+	inst.position = map_to_world(world_to_map(Vector2((posCase.x + 1) * CASE_WIDTH, (posCase.y + 1) * CASE_WIDTH)))
 	
 	inst.grid = self
 	
 	add_child(inst)
 	
+	matrix[posCase.x][posCase.y] = inst
 	
-	inst.nucleon_type = inst.TYPES.proton if inst.check_neighbors() else inst.set_nucleon_type(inst.TYPES.neutron)
+	if inst.can_be_placed():
+		inst.set_nucleon_type(inst.TYPES.proton)
+	else:
+		inst.set_nucleon_type(inst.TYPES.neutron)
+
+
+
+func get_case_of(elt : Nucleon):
+	for x in range(SIDE_LENGTH):
+		for y in range(SIDE_LENGTH):
+			if(matrix[x][y] == elt):
+				return Vector2(x, y)
+	return null
+
+func get_element_at_pos(pos : Vector2):
+	return matrix[pos.x][pos.y]
+	
+func _draw():
+	var i = 1
+	while(i < SIDE_LENGTH):
+	#	draw_line(Vector2(CASE_WIDTH * i, 0), Vector2(CASE_WIDTH * i, 1920), Color(255, 0, 0), 1)
+	#	draw_line(Vector2(0, CASE_WIDTH * i), Vector2(1080, CASE_WIDTH * i), Color(255, 0, 0), 1)
+		draw_line(Vector2(85 * i, 0), Vector2(85 * i, 1920), Color(255, 0, 0))
+		draw_line(Vector2(0, 85 * i), Vector2(1080, 85 * i), Color(255, 0, 0))
+		i += 1
--- nodes/Nucleon.gd
+++ nodes/Nucleon.gd
@@ -12,9 +12,12 @@ export (Texture) var proton_sprite
 
 onready var grid : TileMap = get_parent()
 
+#func _init(posCase : Vector2):
+#	var case : Vector2 = posCase
+
 func _process(delta):
 	if Engine.editor_hint:
-		_editor_process(delta)
+		_editor_process(delta)	
 		return
 	update()
 
@@ -22,27 +25,73 @@ func _editor_process(delta):
 	pass
 
 
-# If there's more than 1 proton next to a proton, it cannot be placed
-func check_neighbors():
-	if nucleon_type == TYPES.neutron: return true
+func get_neighbors(not_used_dir=0):
+	var case = grid.get_case_of(self)
 	
-	var grid_pos : Vector2 = grid.world_to_map(position)
-	var pos_to_check = [
-		Vector2(0, 1),
-		Vector2(0, -1),
-		Vector2(1, 0),
-		Vector2(-1, 0)
-	]
-	var neighbor_protons = 0
+	var neighbors_pos = []
 	
-	for pos in pos_to_check:
-		$Raycast.cast_to = position + (pos * grid.cell_size)
-		if $Raycast.is_colliding() && $Raycast.get("nucleon_type") && $Raycast.get_collider().nucleon_type == TYPES.proton:
-			neighbor_protons += 1
+	if(case.x > 0 and not_used_dir != 3):
+		neighbors_pos.append(Vector2(case.x - 1, case.y))
+	if(case.x < grid.SIDE_LENGTH - 1 and not_used_dir != 4):
+		neighbors_pos.append(Vector2(case.x + 1, case.y))
+	if(case.y > 0 and not_used_dir != 2):
+		neighbors_pos.append(Vector2(case.x, case.y - 1))
+	if(case.y < grid.SIDE_LENGTH - 1 and not_used_dir != 1):
+		neighbors_pos.append(Vector2(case.x, case.y + 1))
 	
-	print(neighbor_protons)
-	return neighbor_protons <= 1
+	var neighbors = []
+	
+	for pos in neighbors_pos:
+		var neighbor = grid.get_element_at_pos(pos)
+		if(neighbor != null): neighbors.append(neighbor)
+	
+	return neighbors
 
+func get_nb_neighbor_proton(not_used_dir=0):
+	var nb_neighbor_protons = 0
+	
+	var neighbors = get_neighbors(not_used_dir)
+	for neighbor in neighbors:
+		if(neighbor.nucleon_type == TYPES.proton):
+			nb_neighbor_protons += 1
+	
+	return nb_neighbor_protons
+
+# If there's more than 1 proton next to a proton, it cannot be placed
+func can_be_placed():
+	if(nucleon_type == TYPES.neutron): return true
+	
+	var nb_neighbor_protons = get_nb_neighbor_proton()
+	if(nb_neighbor_protons > 1): return false
+	
+	var neighbors = get_neighbors()
+	for neighbor in neighbors:
+		if(neighbor.nucleon_type == TYPES.proton):
+			var not_used_dir
+			var my_pos = grid.get_case_of(self)
+			var neighbor_pos = grid.get_case_of(neighbor)
+			if(my_pos.x < neighbor_pos.x):
+				not_used_dir = 3
+			elif(my_pos.x > neighbor_pos.x):
+				not_used_dir = 4
+			elif(my_pos.y < neighbor_pos.x):
+				not_used_dir = 2
+			elif(my_pos.y > neighbor_pos.x):
+				not_used_dir = 1
+			return neighbor.can_be_placed_recursivity(not_used_dir)
+	
+	return true
+
+""" not_used_dir
+none : 0
+bottom : 1
+top : 2
+left : 3
+right : 4
+"""
+func can_be_placed_recursivity(not_used_dir : int):
+	var nb_neighbor_protons = get_nb_neighbor_proton()
+	return nb_neighbor_protons <= 1
 
 """ Setters """
 
@@ -57,5 +106,5 @@ func set_nucleon_type(value, assign=true):
 func get_class(): return "Nucleon"
 
 
-func _draw():
-	draw_line(position, $Raycast.cast_to, Color.blue)
+"""func _draw():
+	draw_line(position, $Raycast.cast_to, Color.blue)"""
